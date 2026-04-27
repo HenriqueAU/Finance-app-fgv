fc1c121e1eba3885de2366622de1e0fdd052f981
Giovanna Soares <gasoarexx@gmail.com>
2026-04-27 12:11:18 -0300

    feat: finaliza tela de parcelamentos com barra de progresso dinâmica

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { DashboardComponent as Dashboard} from './features/dashboard/dashboard/dashboard';
 import { FixedExpensesComponent } from './features/fixed-expenses/fixed-expenses/fixed-expenses';
+import { InstallmentsComponent } from './features/installments/installments/installments';
 //import { AuthGuard } from './core/guards/auth.guard'; // <-- Importe o AuthGuard
 
 export const routes: Routes = [
   { path: 'login', component: Login }, 
   { path: 'register', component: Register }, 
   { path: 'dashboard', component: Dashboard, }, //canActivate: [AuthGuard] }, // <-- Protege a rota do dashboard
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, 
-  { path: 'contas-fixas', component: FixedExpensesComponent } 
+  { path: 'contas-fixas', component: FixedExpensesComponent },
+  { path: 'parcelamentos', component: InstallmentsComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
